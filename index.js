989f626bd8621dab38ada79b785e6e156f1972dd
vivienlee00 <vivien.lee.601@gmail.com>
2018-03-26 07:12:51 -0400

    done

--- index.js
+++ index.js
@@ -6,44 +6,46 @@ K #14: You Are Smarter Than the President*
  **/
 
 
-document.getElementById("change").addEventListener("click", function() {if (year == 1973) { year = 1950; makegraph();}});
+document.getElementById("change").addEventListener("click", function() {if (year == 1973) { year = 1950; barUpdate = bar.data(nums2); makegraph();}});
 
 
 //1973
-var year = 1973
-    var labels1 = ["SS, Unemployment and Labor", "Medicare and Health", "Veterans' Benefits", "Transportation", "Education Training, Employment, and Social services", "Natural Resources", "Science, space, tech", "National Defense"];
-var nums1 = [302, 67.9, 46.9, 35.4, 49.6, 18.6, 15.7, 229] ; 
+var year = 1973;
+var labels1 = ["SS, Unemployment and Labor", "Medicare and Health", "Veterans' Benefits", "Transportation", "Education Training, Employment, and Social services", "Natural Resources", "Science, space, tech", "National Defense"];
+var nums1 = [302, 67.9, 46.9, 35.4, 49.6, 18.6, 15.7, 299] ;
+var labels2 = ["SS, Unemployment and Labor", "Medicare and Health", "Veterans' Benefits", "Transportation", "Education Training, Employment, and Social services", "Natural Resources", "Science, space, tech", "National Defense"];
+var nums2 = [814, 432, 57.7, 57.5, 66, 30.7, 22.8, 361] ;
 
 
 var chart = d3.select(".chart");
 var bar = chart.selectAll("div");
-
-if (year == 1973){
-barUpdate = bar.data(nums1);
-}
-else {
-    barUpdate = bar.data(nums2);
-}
-
-
+var barUpdate = bar.data(nums1);
 var barEnter = barUpdate.enter().append("div");
 
 var makegraph = function(e){
 
+  var chart = d3.select(".chart");
+  var bar = chart.selectAll("div");
+
     if (year == 1973){
-	barEnter.text(function(d, i){
-		return labels1[i] + " " +  d + " trillion dollars";
+      barUpdate = bar.data(nums1);
+	     barEnter.text(function(d, i){
+		       return labels1[i] + " " +  d + " billion dollars";
 	    });
+      barEnter.transition().duration(5000).style("width", function(d) {
+        return d * 4 + "px";
+    });
     }
     else {
-	barEnter.text(function(d, i){
-		return labels2[i] + d;
+      barUpdate = bar.data(nums2);
+	     barEnter.text(function(d, i){
+		       return labels2[i] + " " + d + " billion dollars";
 	    });
+      barEnter.transition().duration(5000).style("width", function(d) {
+        return d * 4 + "px";
+    });
     }
-
-    barEnter.transition().duration(5000).style("width", function(d) {
-	    return d * 4 + "px";
-	});
+    console.log(year);
 };
 
 
